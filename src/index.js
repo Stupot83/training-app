2e474046ac07de40be37bfafd06244bfe6787fa0
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2021-01-20 16:15:50 +0000

    Finish sections 1 and 2 of training

--- src/index.js
+++ src/index.js
@@ -8,11 +8,20 @@ if (module.hot) {
 
 // Create a React component
 const App = () => {
-    return <div>
-        <label class='label' for='name'>Enter name:</label>
-        <input id='name' type='text' />
-        <button style='background-color: blue; color: white;'>Submit</button>
-    </div>;
+    const buttonText = { text: 'Click Me!' };
+    const labelText = 'Enter Name:';
+
+    return (
+        <div>
+            <label className='label' htmlFor='name'>
+                {labelText}
+            </label>
+            <input id='name' type='text' />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText.text}
+            </button>
+        </div>
+    )
 };
 
 // Take the React component and show it on the screen
