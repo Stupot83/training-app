5040ade3ee4ca4e348040dd74fa82bb14c716a1f
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2021-01-27 11:32:20 +0000

    Add further changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 if (module.hot) {
     module.hot.accept();
 }
@@ -9,21 +12,31 @@ if (module.hot) {
 // Create a React component
 const App = () => {
     return (
-        <div className='ui container comments'>
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                <img alt="avatar" src='https://source.unsplash.com/random' />
-                </a>
-                <div className='content'>
-                    <a href='/' className='author'>
-                        Sam
-                </a>
-                    <div className='metadata'>
-                        <span className='date'>Today at 6:00PM</span>
-                    </div>
-                    <div className='text'>Nice blog post!</div>
-                </div>
-            </div>
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                    author="Boris"
+                    time="Today at 4.45PM"
+                    post="I am loving React!"
+                    image='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Michael"
+                    time="Today at 5.45PM"
+                    post="I am also loving React!"
+                    image='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Matt"
+                    time="Today at 6.45PM"
+                    post="I am loving React more!"
+                    image='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
         </div>
     )
 }
